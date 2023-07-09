a239d901adbf9a96a8b02972ebe9f148eb9b7a78
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-09 22:44:00 +0300

    deleting the old course

--- lesson2/HM1/assets/js/hello.js
+++
@@ -1,4 +0,0 @@
-
-const name = prompt("What is your name?", "John");
-
-alert(`Hello, ${name}! How are you?`);
--- lesson4/HM5/assets/js/task10.js
+++
@@ -1,9 +0,0 @@
-
-//Надрукувати повну таблицю множення від 1 до 10
-
-for (let i = 1; i < 10; i++) {
-    for (let j = 1; j < 10; j++) {
-        document.write(`${i} * ${j} = ${i * j} <p>`);
-    }
-    document.write("<br>")
-}
