7ad8177d0235a293ab3311b786ceecf3f50b3c92
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-09 21:25:58 +0300

    14, 15

--- lesson10/HM14/js/tasks/task1.js
+++ lesson10/HM14/js/tasks/task1.js
@@ -4,13 +4,22 @@
 // Все це із замиканнями, наприклад: sum(3) = 3 sum(5) = 8 sum(20) = 28
 
 
-function calculate(value){
-
-
-    
-
-}
-
+function createSum() {
+    let sum = 0;
+  
+    return function (num) {
+      sum += num;
+      return sum;
+    };
+  }
+  
+  // Приклад використання
+  
+  const sum = createSum();
+  
+  console.log(sum(3)); // 3
+  console.log(sum(5)); // 8
+  console.log(sum(20)); // 28
 
 function processFirstTask() {
 
