eb678da38760499d1162bad26b19450d55bd4e88
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-13 14:51:11 +0300

    refactor

---
+++ HM25/js/main.js
@@ -0,0 +1,18 @@
+
+const GOOGLE_URL = "http://www.google.com"
+
+const userUrl = prompt("Укажите ссылку для перехода: ");
+
+const userButton = document.getElementById('user-button');
+userButton.addEventListener('click', () => redirectToUrl(userUrl));
+
+const googleButton = document.getElementById('google-button');
+googleButton.addEventListener('click', () => redirectToUrl(GOOGLE_URL));
+
+
+function redirectToUrl(url) {
+    if (!url.startsWith('http://') || !url.startsWith('https://')) {
+        url = 'http://' + url;
+    }
+    window.location.href = url;
+}
