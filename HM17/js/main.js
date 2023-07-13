0e4ffcf4aab152de8322e46feae788f3b4ae2cfe
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-13 11:14:35 +0300

    refactor

--- HM17/js/main.js
+++ HM17/js/main.js
@@ -2,18 +2,18 @@
 import Person from "../js/person.js";
 import Car from "../js/car.js";
 
-console.log("---------------");
+console.log("----------------");
 
 const sergey = new Person("Sergey", 20);
 const tesla = new Car("Tesla", "Model X", 2015, "АА1111АА");
 tesla.owner = sergey;
 tesla.print();
 
-console.log("---------------");
+console.log("----------------");
 
 const oleg = new Person("Oleg", 13);
 const mustang = new Car("Ford Mustang", "GT VI", 2014, "BH0000АI");
 mustang.owner = oleg;
 mustang.print();
 
-console.log("---------------");
+console.log("----------------");
