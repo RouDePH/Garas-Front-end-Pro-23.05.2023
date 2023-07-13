e115c4a8920f1d5cf5a70e98eb44095153a4e770
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-13 17:23:58 +0300

    refactor

---
+++ HM27/assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+window.onscroll = function () { myFunction() };
+
+const header = document.getElementById("header");
+const sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
