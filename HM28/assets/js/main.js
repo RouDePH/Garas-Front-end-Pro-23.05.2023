516835f027647f27d7abb5e4324149fd8002c98a
Гарас Сергей Ярославович <sergeygaras2002@gmail.com>
2023-07-20 13:59:01 +0300

    28

--- HM28/assets/js/main.js
+++ HM28/assets/js/main.js
@@ -1,16 +1,32 @@
 
-// Пишемо свій слайдер зображень.
 
-// Відображаємо зображення та кнопки Next, Prev з боків від зображення.
-// При кліку на Next - показуємо наступне зображення.
-// При кліку на Prev - попереднє .
-// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev.
+const images = ["image-1.jpg", "image-2.jpg", "image-3.jpg", "image-4.jpg", "image-5.jpg", "image-6.jpg", "image-7.jpg", "image-8.jpg", "image-9.jpg", "image-10.jpg"];
 
+let currentImageIndex = 0;
+document.getElementById("prev-button").style.display = "none";
 
+function changeImage(index) {
+    document.getElementById("slider-image").src = `assets/images/${images[index]}`;
 
+    if (index === 0) {
+        document.getElementById("prev-button").style.display = "none";
+    } else {
+        document.getElementById("prev-button").style.display = "block";
+    }
 
-// while(true){
-//     switch(){
+    if (index === images.length - 1) {
+        document.getElementById("next-button").style.display = "none";
+    } else {
+        document.getElementById("next-button").style.display = "block";
+    }
+}
 
-//     }
-// }
+document.getElementById("prev-button").addEventListener("click", function () {
+    currentImageIndex--;
+    changeImage(currentImageIndex);
+});
+
+document.getElementById("next-button").addEventListener("click", function () {
+    currentImageIndex++;
+    changeImage(currentImageIndex);
+});
